6048ef23cfc5906574a30133b0bb25e49792e679
David Young <roboguy06@gmail.com>
2023-02-06 21:13:01 -0600

    Always use cabal executable in fun-suslik.sh

--- fun-suslik.sh
+++ fun-suslik.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-if [ -d ".stack-work/dist" ]; then
-  # Use a stack executable, if it exists
-  stack exec fun-suslik $1
-else
-  echo Executable not compiled by stack. Run stack build.
-  echo If you want to use cabal, run \'cabal exec fun-suslik \<file\>.fsus\'
-fi
+cabal exec fun-suslik $1
